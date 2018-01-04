ded62540601f778700a7cc68b29027f4161acc6d
lizheming <secretlzm007@gmail.com>
2018-01-05 00:26:13 +0800

    add main file

---
+++ index.js
@@ -0,0 +1,19 @@
+const render = require('drone-render');
+const request = require('request-promise-native');
+
+const {
+  PLUGIN_SCKEY,
+  PLUGIN_TITLE,
+  PLUGIN_MESSAGE,
+  SERVER_CHAN_KEY
+} = process.env;
+
+const SCKEY = PLUGIN_SCKEY || SERVER_CHAN_KEY;
+
+request({
+  url: `https://sc.ftqq.com/${SCKEY}.send`,
+  qs: {
+    text: PLUGIN_TITLE,
+    desp: render(PLUGIN_MESSAGE)
+  }
+});
